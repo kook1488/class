c5316a1c29b98cc39e2e21693cf139c856d98ca3
[APP1] 국한 <kook1488@naver.com>
2024-08-12 22:44:20 +0900

    8.12

--- lib/codefactory/homescreen.dart
+++ lib/codefactory/homescreen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:histudy/const/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        //아래도 적용됨
+        // art enter 로 감싸주기
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: colors
+                .map(
+                  (e) => Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: e,
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+      ),
+    );
   }
 }
+//ㄴㅇㄴㅇ 
